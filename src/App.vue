ae2e6841c675c01106deac2fcd3db4fd5cfdb548
designeng <denissavenok@gmail.com>
2021-01-10 16:08:44 +0300

    Import UsersList component & render. Add src/components/UsersList

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
-        Vue.js App is rendered!
+        <h3>Vue.js App is rendered!</h3>
+        <UsersList />
     </div>
 </template>
+
+<script>
+import UsersList from '@/components/UsersList';
+
+export default {
+    name: 'App',
+    components: {
+        UsersList
+    }
+}
+</script>
---
+++ src/components/UsersList.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul class="users-list">
+        <li v-for="(name, index) in users">{{ name }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'UsersList',
+    data () {
+        return {
+            users: [
+                'Alice',
+                'Robert',
+                'John',
+                'Lucy',
+                'Michael'
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+ul.users-list {
+    list-style-type: none;
+}
+</style>
