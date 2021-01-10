7f421f5dfb0602767845ec48c7bf4024cc2a312b
designeng <denissavenok@gmail.com>
2021-01-10 17:24:49 +0300

    Add src/components/users/ - move UsersList

---
+++ src/components/users/Users.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <UsersList />
+    </div>
+</template>
+
+<script>
+import UsersList from '@/components/users/UsersList';
+
+export default {
+    name: 'Users',
+    components: {
+        UsersList
+    }
+}
+</script>
