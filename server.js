6adf55f9ba5bfb219d06f5a2d6fefe2b549848f2
designeng <denissavenok@gmail.com>
2021-01-11 11:45:19 +0300

    Insert client script in index.html, response with html, get script source params from webpack config

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('console-stamp')(console, 'HH:MM:ss.l');
 
+const fs = require('fs');
+const moment = require('moment');
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
@@ -10,6 +12,10 @@ const app = express();
 const compiler = webpack(config);
 
 const PORT = 8081;
+const DELIMITER = '</body>';
+
+var indexContent = fs.readFileSync(__dirname + '/public/index.html', 'utf-8');
+var indexContentSplitted = indexContent.split(DELIMITER);
 
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
@@ -18,9 +24,14 @@ app.use(webpackDevMiddleware(compiler, {
 app.use(webpackHotMiddleware(compiler));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    var scriptSrc = config.output.publicPath + Object.keys(config.entry)[0] + '.js';
+
+    var html = `${indexContentSplitted[0]}\n<script src="${scriptSrc}"></script>\n${DELIMITER}${indexContentSplitted[1]}`;
+
+    res.set('Content-Type', 'text/html; charset=utf-8');
+    res.status(200).send(html);
 });
 
 app.listen(PORT, function() {
-    console.log('Listening on %a', PORT);
+    console.log('Dev server is running: http://localhost:%j', PORT);
 });
