589e37e85799834d98a5f0e75c1a26680ca471e9
designeng <denissavenok@gmail.com>
2021-01-12 21:17:55 +0300

    Add src/components/users/AddUser.vue

---
+++ src/components/users/AddUser.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>Add user</h3>
+        <form>
+            <input type="text" />
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'AddUser',
+    created () {
+        console.log('ADD_USER');
+    }
+}
+</script>
