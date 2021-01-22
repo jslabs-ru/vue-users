14b7b3527e43138bcaaf19bee921b12cb0c874b9
designeng <denissavenok@gmail.com>
2021-01-22 17:37:14 +0300

    Change route: /users/add_user -> /admin/add_user. Add route /users/:user - render UserAccount

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/components/main/Home.vue';
 import Users from '@/components/users/Users.vue';
+import UserAccount from '@/components/users/UserAccount.vue';
 import AddUser from '@/components/users/AddUser.vue';
 import NotFound from '@/components/errors/404.vue';
 
@@ -17,7 +18,11 @@ const routes = [
         component: Users
     },
     {
-        path: '/users/add_user',
+        path: '/users/:user',
+        component: UserAccount
+    },
+    {
+        path: '/admin/add_user',
         component: AddUser
     },
     {
