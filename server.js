5ccd0ddc6ea280a6c4c67eeb168a8b68c5ab693a
designeng <denissavenok@gmail.com>
2021-01-22 12:47:30 +0300

    Require connect-history-api-fallback as history, create historyMiddleware. Add function rewriteIndex & call it before server run. Add exception for /api/v1 requests - in all other cases use historyMiddleware. Create staticFileMiddleware & use in app, get static files from public folder

--- server.js
+++ server.js
@@ -3,19 +3,30 @@ require('console-stamp')(console, 'HH:MM:ss.l');
 const fs = require('fs');
 const moment = require('moment');
 const express = require('express');
+const history = require('connect-history-api-fallback');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./src/webpack.config.js');
 
 const app = express();
+const staticFileMiddleware = express.static('public');
 const compiler = webpack(config);
 
 const PORT = 8081;
-const DELIMITER = '</body>';
 
-var indexContent = fs.readFileSync(__dirname + '/public/index.html', 'utf-8');
-var indexContentSplitted = indexContent.split(DELIMITER);
+function rewriteIndex() {
+    const DELIMITER = '</body>';
+    var indexContent = fs.readFileSync(__dirname + '/tpl/index-tpl.html', 'utf-8');
+    var indexContentSplitted = indexContent.split(DELIMITER);
+
+    var scriptSrc = config.output.publicPath + Object.keys(config.entry)[0] + '.js';
+
+    var html = `${indexContentSplitted[0]}\n<script src="${scriptSrc}"></script>\n${DELIMITER}${indexContentSplitted[1]}`;
+    fs.writeFileSync(__dirname + '/public/index.html', html);
+}
+
+rewriteIndex();
 
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
@@ -23,14 +34,20 @@ app.use(webpackDevMiddleware(compiler, {
 
 app.use(webpackHotMiddleware(compiler));
 
-app.get('/', function(req, res) {
-    var scriptSrc = config.output.publicPath + Object.keys(config.entry)[0] + '.js';
-
-    var html = `${indexContentSplitted[0]}\n<script src="${scriptSrc}"></script>\n${DELIMITER}${indexContentSplitted[1]}`;
-
-    res.set('Content-Type', 'text/html; charset=utf-8');
-    res.status(200).send(html);
+const historyMiddleware = history({
+    disableDotRule: true,
+    verbose: true
 });
+
+app.use((req, res, next) => {
+    if (req.path.match(/^\/api\/v1\//)) {
+        next();
+    } else {
+        historyMiddleware(req, res, next);
+    }
+});
+
+app.use(staticFileMiddleware);
 
 app.get('/api/v1/users', function(req, res) {
     res.json([
