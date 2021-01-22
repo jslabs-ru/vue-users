504f2a9aa002964aa4dc21143bf56fafde9242cc
designeng <denissavenok@gmail.com>
2021-01-22 17:37:33 +0300

    Add src/components/users/UserAccount.vue

---
+++ src/components/users/UserAccount.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ user }} account
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'UserAccount',
+    components: {
+
+    },
+    computed: {
+        user () {
+            return this.$route.params['user'];
+        }
+    }
+}
+</script>
