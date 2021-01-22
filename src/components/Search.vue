8a99932a99b043a4bdc2bdb9d2a4e26606075608
designeng <denissavenok@gmail.com>
2021-01-22 17:38:57 +0300

    Add src/utils folder with noop function

---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2"
+                placeholder="Search"
+                v-model="user"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0"
+                v-on:click.prev="onSearchSubmit"
+            >Search</b-button>
+        </b-nav-form>
+    </b-navbar-nav>
+</template>
+
+<script>
+import noop from '@/utils/noop';
+export default {
+    data () {
+        return {
+            user: ''
+        }
+    },
+    methods: {
+        onSearchSubmit () {
+            this.$router.push({path: `/users/${this.user}`}, noop)
+        }
+    }
+}
+</script>
