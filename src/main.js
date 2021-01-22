29176c08ff11d80250d9e4bddd7ae667670ad6ef
designeng <denissavenok@gmail.com>
2021-01-22 12:01:02 +0300

    Install bootstrap-vue & use in vue app. Import styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
+
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
 
 new Vue({
     render: (h) => h(App),
