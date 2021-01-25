b0fa33121b144d24e2e5fd48ceee456f68c62c09
designeng <denissavenok@gmail.com>
2021-01-25 19:02:37 +0300

    Use b-container as App root element - remove extra div wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-        <b-container>
-            <b-navbar toggleable="lg" type="dark" variant="info">
-                <b-navbar-brand to="/">App</b-navbar-brand>
+    <b-container>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand to="/">App</b-navbar-brand>
 
-                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-                <b-collapse id="nav-collapse" is-nav>
-                    <b-navbar-nav>
-                        <b-nav-item to="/users">Users</b-nav-item>
-                    </b-navbar-nav>
-                    <Search />
-                </b-collapse>
-            </b-navbar>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item to="/users">Users</b-nav-item>
+                </b-navbar-nav>
+                <Search />
+            </b-collapse>
+        </b-navbar>
 
-            <RouterView />
-        </b-container>
-    </div>
+        <RouterView />
+    </b-container>
 </template>
 
 <script>
