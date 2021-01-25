8c91738d4bc5356d4b94b0976eabc5e50784b126
designeng <denissavenok@gmail.com>
2021-01-25 18:50:27 +0300

    Add component src/components/playground/NextTick.vue

---
+++ src/components/playground/NextTick.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="alert alert-warning" role="alert">
+            {{ status }}
+        </div>
+        <span ref="message-container">{{ message }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NextTick',
+    data () {
+        return {
+            status: '',
+            message: 'Not updated'
+        }
+    },
+    mounted () {
+        this.status = this.$refs['message-container'].textContent;
+        this.updateMessage();
+
+        this.$nextTick(() => {
+            this.status = this.$refs['message-container'].textContent;
+        })
+    },
+    methods: {
+        updateMessage () {
+            this.message = 'Updated';
+        }
+    }
+}
+</script>
