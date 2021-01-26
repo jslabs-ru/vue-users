19c47eb641d48f569437dff694548a9b00e036c0
designeng <denissavenok@gmail.com>
2021-01-26 13:15:04 +0300

    Add src/components/playground/Playground.vue

---
+++ src/components/playground/Playground.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul>
+        <li v-for="(item, index) in links">
+            <router-link v-bind:to="item.path">{{ item.name }}</router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'Playground',
+    data () {
+        return {
+            links: [
+                {
+                    path: '/playground/array_reactivity',
+                    name: 'ArrayReactivity'
+                },
+                {
+                    path: '/playground/next_tick',
+                    name: 'NextTick'
+                },
+                {
+                    path: '/playground/render_function',
+                    name: 'RenderFunction'
+                }
+            ]
+        }
+    }
+}
+</script>
