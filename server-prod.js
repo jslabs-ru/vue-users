2fb09289e43e5bf079bb5f66ae920b468e77dc4c
designeng <denissavenok@gmail.com>
2021-01-26 21:17:12 +0300

    Add server-prod.js. Move express & connect-history-api-fallback in app dependencies

---
+++ server-prod.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const express = require('express');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+const staticFileMiddleware = express.static('dist');
+
+const PORT = process.env.PORT || 8080;
+
+const historyMiddleware = history({
+    disableDotRule: true,
+    verbose: true
+});
+
+app.use(staticFileMiddleware);
+
+app.use((req, res, next) => {
+    if (req.path.match(/^\/api\/v1\//)) {
+        next();
+    } else {
+        historyMiddleware(req, res, next);
+    }
+});
+
+app.use(staticFileMiddleware);
+
+app.get('/api/v1/users', function(req, res) {
+    res.json([
+        'Alice',
+        'Robert',
+        'John',
+        'Lucy',
+        'Michael'
+    ]);
+})
+
+app.listen(PORT, function() {
+    console.log('Server is running: http://localhost:%j', PORT);
+});
