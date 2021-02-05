d86041142bf1118af702b7964834e818311ffe81
designeng <denissavenok@gmail.com>
2021-02-05 13:42:41 +0300

    Use Auth0Plugin with auth config settings & onRedirectCallback

--- src/main.js
+++ src/main.js
@@ -2,11 +2,35 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
+import { Auth0Plugin } from './auth';
 
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 Vue.use(BootstrapVue);
+
+const [
+    domain,
+    clientId,
+    audience
+] = [
+    DOMAIN,
+    CLIENT_ID,
+    AUDIENCE
+];
+
+Vue.use(Auth0Plugin, {
+    domain,
+    clientId,
+    audience,
+    onRedirectCallback: appState => {
+        router.push(
+            appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+        )
+    }
+});
 
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
