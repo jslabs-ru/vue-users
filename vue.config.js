c717decaf4af3304e676cdbe98fac5b7c441d6c0
designeng <denissavenok@gmail.com>
2021-02-05 13:41:48 +0300

    Add option chainWebpack: use define plugin for authConfig.json data substitution

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
+const path = require('path');
+
+function resolve (dir) {
+    return path.join(__dirname, dir);
+}
+
+const { domain, clientId, audience } = require('./authConfig.json');
+
 module.exports = {
     devServer: {
         proxy: 'http://localhost:8888'
+    },
+    chainWebpack: config => {
+        config.plugin('define').tap(definitions => {
+            definitions[0] = Object.assign(definitions[0], {
+                DOMAIN: JSON.stringify(domain),
+                CLIENT_ID: JSON.stringify(clientId),
+                AUDIENCE: JSON.stringify(audience)
+            })
+            return definitions;
+        })
     }
 }
