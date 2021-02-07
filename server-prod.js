e1cc581fb6ac02ef2f6894ca5d55e316e805d10e
designeng <denissavenok@gmail.com>
2021-02-07 19:02:58 +0300

    Add message event handler - stop server on shutdown

--- server-prod.js
+++ server-prod.js
@@ -34,6 +34,15 @@ app.get('/api/v1/users', function(req, res) {
     ]);
 })
 
-app.listen(PORT, function() {
+const server = app.listen(PORT, function() {
     console.log('Server is running: http://localhost:%j', PORT);
+});
+
+process.on('message', (message) => {
+    if (message === 'shutdown') {
+        console.log('Stop express server');
+        server.close(() => {
+            process.exit();
+        });
+    }
 });
