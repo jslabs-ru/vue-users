92f3948eede39ac59e1edcb105aad4d91694358e
designeng <denissavenok@gmail.com>
2021-02-11 21:07:07 +0300

    Collect all tasks data after drag & drop in TasksStagesPanel. Refactor TasksList

--- src/components/tasks/TasksList.vue
+++ src/components/tasks/TasksList.vue
@@ -1,31 +1,28 @@
 <template>
-    <table class="table table-bordered" v-if="tasks.length">
-        <thead>
-            <tr>
-                <th scope="col" class="pointer">Task name</th>
-            </tr>
-        </thead>
-            <draggable
-                tag="tbody"
-                :list="list"
-                v-model="tasksDragModel"
-                group="tasks"
-                draggable=".table-row"
-                @end="onDragEnd"
-            >
-                <tr class="table-row"
-                    v-for="(item, index) in list"
-                >
-                    <td scope="row" class="grabbable">
-                        <span class="pointer">{{ item.name }}</span>
-                    </td>
-                    <!-- <td scope="row"
-                        class="open-category-cell pointer"
-                        @click="openTaskPage(item.id)"
-                    ></td> -->
-                </tr>
-            </draggable>
-    </table>
+    <draggable
+        :list="list"
+        @end="onDragEnd"
+        class="list-group"
+        draggable=".item"
+        group="tasks"
+    >
+        <div
+          class="list-group-item item"
+          v-for="element in list"
+          :key="element.name"
+        >
+          {{ element.name }}
+        </div>
+
+        <div
+          slot="header"
+          class="btn-group list-group-item list-group-header"
+          role="group"
+          aria-label="Basic example"
+        >
+            {{ stageName }}
+        </div>
+    </draggable>
 </template>
 
 <script>
@@ -35,16 +32,17 @@ export default {
     name: 'TasksList',
     data () {
         return {
-            tasks: [
-                {id: 0, name: 'task 1'},
-                {id: 1, name: 'task 2'},
-                {id: 2, name: 'task 3'},
-            ]
         }
     },
     props: {
+        stageName: {
+            type: String
+        },
         list: {
             type: Array
+        },
+        trackTasks: {
+            type: Function
         }
     },
     components: {
@@ -65,8 +63,14 @@ export default {
 
         },
         onDragEnd () {
-
+            this.trackTasks()
         }
     }
 }
 </script>
+
+<style>
+.list-group-header {
+    background-color: #FFCC99;
+}
+</style>
--- src/components/tasks/TasksStagesPanel.vue
+++ src/components/tasks/TasksStagesPanel.vue
@@ -2,17 +2,30 @@
     <div class="row">
         <div class="col">
             <TasksList
+                :stageName="stages[0]"
                 :list='stageOneList'
+                :trackTasks="trackTasks"
             />
         </div>
         <div class="col">
             <TasksList
+                :stageName="stages[1]"
                 :list='stageSecondList'
+                :trackTasks="trackTasks"
             />
         </div>
         <div class="col">
             <TasksList
+                :stageName="stages[2]"
                 :list='stageThirdList'
+                :trackTasks="trackTasks"
+            />
+        </div>
+        <div class="col">
+            <TasksList
+                :stageName="stages[3]"
+                :list='stageForthList'
+                :trackTasks="trackTasks"
             />
         </div>
     </div>
@@ -22,27 +35,51 @@
 import TasksList from '@/components/tasks/TasksList.vue';
 import Draggable from 'vuedraggable';
 
+const [stageOneList, stageSecondList, stageThirdList, stageForthList] = [
+    'stageOneList',
+    'stageSecondList',
+    'stageThirdList',
+    'stageForthList',
+]
+
 export default {
     name: 'TasksStagesPanel',
     data () {
         return {
-            stageOneList: [
+            stages: [
+                'Consideration',
+                'Development',
+                'Testing',
+                'Deploy',
+            ],
+            [stageOneList]: [
                 {id: 0, name: 'task 1'},
                 {id: 1, name: 'task 2'},
                 {id: 2, name: 'task 3'},
             ],
-            stageSecondList: [
+            [stageSecondList]: [
                 {id: 3, name: 'task 4'},
                 {id: 4, name: 'task 5'},
                 {id: 5, name: 'task 6'},
                 {id: 6, name: 'task 7'},
             ],
-            stageThirdList: [
+            [stageThirdList]: [
+            ],
+            [stageForthList]: [
             ],
         }
     },
     components: {
         TasksList
+    },
+    methods: {
+        trackTasks () {
+            /* do smth with tasks */
+            [stageOneList, stageSecondList, stageThirdList, stageForthList].forEach((item, i) => {
+                console.log(this[item].length);
+            });
+
+        }
     }
 }
 </script>
