a31ffa2e67fa9f442a17bc33915e4405e7f4108f
designeng <denissavenok@gmail.com>
2021-02-12 16:22:17 +0300

    Use @ in path

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import App from './App.vue';
-import router from './router';
-import { Auth0Plugin } from './auth';
+import App from '@/App.vue';
+import router from '@/router';
+import { Auth0Plugin } from '@/auth';
 
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
