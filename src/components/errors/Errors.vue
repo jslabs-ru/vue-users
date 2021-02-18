00273a78a77608566a83831a6e4b9bc06c3dc5ab
designeng <denissavenok@gmail.com>
2021-02-18 18:44:51 +0300

    Add src/components/errors/Errors.vue

---
+++ src/components/errors/Errors.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="alert alert-danger" role="alert">{{ JSON.stringify(errors) }}</div>
+</template>
+
+<script>
+export default {
+    name: 'Errors',
+    data () {
+        return {
+            errors: ['Error 1', 'Error 2']
+        }
+    }
+}
+</script>
