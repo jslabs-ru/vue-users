78c27d0753287c3cb59c854f8f8c3f1724ad95c7
designeng <denissavenok@gmail.com>
2021-02-20 16:04:59 +0300

    Add beforeRouteEnter hook: fetch all users & provide in UsersList

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -1,17 +1,37 @@
 <template>
     <div>
         <h5>Our users:</h5>
-        <UsersList />
+        <UsersList v-bind:users="users" />
     </div>
 </template>
 
 <script>
+import UserService from '@/services/userService';
+
 import UsersList from '@/components/users/UsersList.vue';
 
 export default {
     name: 'Users',
     components: {
         UsersList
+    },
+    data () {
+        return {
+            users: []
+        }
+    },
+    beforeRouteEnter (to, from, next) {
+        UserService.getAll()
+            .then(users => {
+                next(vm => {
+                    vm.setUsers(users)
+                })
+            })
+    },
+    methods: {
+        setUsers (users) {
+            this.users = users;
+        }
     }
 }
 </script>
