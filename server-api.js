4e4ec9c72b39f8a05f8920bc3300ce9ce2cf1e1e
designeng <denissavenok@gmail.com>
2021-02-25 01:18:38 +0300

    Use createApiRouter: set base api url `/api/v1`

--- server-api.js
+++ server-api.js
@@ -1,36 +1,16 @@
-const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser')
 const knex = require('knex');
+const moment = require('moment');
+
+const createApiRouter = require('./apiRouter');
 
 const app = express();
 
 const PORT = parseInt(process.env.PORT) || 5000;
 const USERS = 'users';
 
-app.get('/api/v1/users', async function(req, res) {
-    const db = app.get('db');
-    const users = await db(USERS);
-    res.json(users);
-})
-
-app.get('/api/v1/users/:userid', async function(req, res) {
-    const db = app.get('db');
-    const { userid } = req.params;
-
-    const users = await db(USERS).where('userid', userid);
-
-    if(users.length) {
-        res.status(200).json(users[0]);
-    } else {
-        res.status(404).json({
-            error: 'Not found'
-        });
-    }
-})
-
-app.post('/api/v1/users', async function(req, res) {
-    res.json({});
-})
+app.use('/api/v1', createApiRouter(app));
 
 async function startServer(db) {
     app.set('db', db);
