1a23dc7c4d88af54a310585d6a20600c17854df5
designeng <denissavenok@gmail.com>
2021-02-24 19:30:49 +0300

    Add src/components/MessagesPannel.vue

---
+++ src/components/MessagesPannel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-if="error" class="alert alert-danger" role="alert">{{ error.message }}</div>
+        <div v-if="warn" class="alert alert-warning" role="alert">{{ warn.message }}</div>
+        <div v-if="info" class="alert alert-info" role="alert">{{ info }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MessagesPannel',
+    props: {
+        warn: {type: Object},
+        error: {type: Object},
+        info: {type: String}
+    }
+}
+</script>
