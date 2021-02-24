1aa1bb2c05de9400ba8c1c7d259b04527724f15f
designeng <denissavenok@gmail.com>
2021-02-25 01:51:56 +0300

    Add src/store/

---
+++ src/store/modules/users.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import UserService from '@/services/userService';
+
+import {
+    FETCH_USERS,
+    FETCH_USER_ACCOUNT_DATA
+} from '@/store/actions.type';
+
+import {
+    SET_USERS
+} from '@/store/mutations.type';
+
+const state = {
+    users: []
+}
+
+const actions = {
+    [FETCH_USERS]: UserService.getAll(),
+    [FETCH_USER_ACCOUNT_DATA]: UserService.getUserAccountData(id),
+}
+
+const mutations = {
+    [SET_USERS] (state, users) {
+        Vue.set('users', users);
+    }
+}
+
+const getters = {
+    usersCount (state) {
+        return state.users.length;
+    }
+}
+
+export default {
+    state,
+    actions,
+    mutations,
+    getters
+}
